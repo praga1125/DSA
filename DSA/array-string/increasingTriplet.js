fbd3a8880b9750fe8eeac32580c295fdc0bc2e1d
praga1125 <rpraga50@gmail.com>
2021-04-08 20:54:15 +0530

    program of increasing triplets subsequence

---
+++ DSA/array-string/increasingTriplet.js
@@ -0,0 +1,26 @@
+function increasingTriplet(arr){
+    let firstNumber = Infinity;
+    let secondNumber = Infinity;
+    for (let currentNumber of arr) {
+      if (currentNumber > secondNumber && currentNumber > firstNumber) {
+        return true;
+      }
+      if (currentNumber > firstNumber) {
+        secondNumber = currentNumber;
+      } else {
+        firstNumber = currentNumber;
+      }
+    }
+    return false;
+  }
+
+  let arr = [1, 2, 10, 5, 2];
+  console.log(`The given array is :`);
+  console.log(arr);
+  let result = increasingTriplet(arr);
+  if(result){
+      console.log(`It is increasing triplet Subsequence`);
+  }
+  else{
+      console.log(`It is not a increasing triplet Subsequence`);
+  }
