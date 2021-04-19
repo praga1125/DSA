284c0fa1e4d6e9668c2eb3f929f76d76f64a1eb2
praga1125 <rpraga50@gmail.com>
2021-04-19 16:34:51 +0530

    program of find the minimum stack value

--- DSA/tree/postOrderTraversal.js
+++ DSA/tree/postOrderTraversal.js
@@ -39,32 +39,29 @@ class binarySearchTree{
     }
 
     postOrder(root){
-        let stack = [root];
-        let result = []
-        if(!root){
-           return [];
-        }
-        while(stack.length > 0) {
-            let node = stack.pop();
-            result.push(node.data);
-            if(node.left){ 
-              stack.push(node.left);
-            }
-            if(node.right){ 
-              stack.push(node.right);
-            }
-        }
-        return result.reverse();
+       let stack = [root];
+       let result = [];
+       while(stack.length > 0){
+           let node = stack.pop();
+           result.push(node.data);
+           if(node.left){
+               stack.push(node.left);
+           }
+           if(node.right){
+               stack.push(node.right);
+           }
+       }
+       return result.reverse();
     }
 }
 
 let obj = new binarySearchTree();
-let arr = [1, 2, 3];
+let arr = [2, 1, 3];
 console.log(`The given BST is : `);
 console.log(arr);
 for(let i of arr){
     obj.insertNode(i);
 }
 let result = obj.postOrder(obj.root);
-console.log(`The postorder for the binary search tree :`);
+console.log(`The postorder traversal for the binary search tree :`);
 console.log(result);
