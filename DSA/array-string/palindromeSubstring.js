b1a0b759f5ad1738f92d9e2b0259f79407e2d838
praga1125 <rpraga50@gmail.com>
2021-04-20 19:26:24 +0530

    program of find the longest palindrome substring

---
+++ DSA/array-string/palindromeSubstring.js
@@ -0,0 +1,30 @@
+function longestPalindrome(str){
+    let len = str.length;
+    while(len > 0){
+    for(let i=0; i<str.length-len+1; i++){
+        let word = str.slice(i, i+len);
+        if(isPalindrome(word)){
+            return word;
+        }
+    }
+    len--;
+  }
+  return "";
+}
+function isPalindrome(arr){
+    let i = 0;
+    let j = arr.length-1;
+    while(i <= j){
+        if(arr[i] !== arr[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+let str = "eeemalayalamooo";
+console.log(`The given string is : ${str}`);
+let result = longestPalindrome(str);
+console.log(`The longest palindrome is : ${result}`);
---
+++ DSA/test.js
@@ -0,0 +1,4 @@
+let str = "hello";
+console.log(str.length);
+let res = str.slice(0,1+5);
+console.log(res);
