efeeb50358882e58e08a4a16e23251a54f826993
praga1125 <rpraga50@gmail.com>
2021-04-27 10:58:44 +0530

    program to find the maximum value of sliding window

---
+++ DSA/array-string/maxSlidingWindow.js
@@ -0,0 +1,26 @@
+function maxSlidingWindow(arr, limit){
+  let indx = [];
+  let result = [];
+  for(let i=0; i<arr.length; i++){
+    while(indx && arr[indx[indx.length-1]] <= arr[i]){
+      indx.pop();
+    }
+    indx.push(i);
+    if(indx[0] == i - limit){
+      indx.shift();
+    }
+    if(i >= limit-1){
+     result.push(arr[indx[0]])  
+  } 
+ }
+ return result;
+}
+
+let arr = [1, 3, -1, -3, 5, 3, 6, 7];
+console.log(`The given array is : `);
+console.log(arr);
+let limit =  3;
+console.log(`The given window limit is : ${limit}`);
+let result = maxSlidingWindow(arr, limit);
+console.log(`The maximum value of  sliding window is : `);
+console.log(result);
