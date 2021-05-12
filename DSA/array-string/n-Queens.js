0159b5cd85b2387f5fe9c4651ac25b2737c4bf76
praga1125 <rpraga50@gmail.com>
2021-05-12 09:13:00 +0530

    simple program of n - Queens puzzle problem

---
+++ DSA/array-string/n-Queens.js
@@ -0,0 +1,22 @@
+function solveNQueens(n){
+  let result = [];
+  backTrack(result, n);
+  return result;
+}
+function backTrack(result, n, board = [], row = 0){
+  if(row == n){
+    result.push(board.map(col => '.'.repeat(col) + 'Q' +'.'.repeat(n-col-1)));  
+    return;
+  }
+  for(let col = 0; col < n; col++){
+    if(!board.some((itemCol, itemRow) => itemCol == col || itemCol + itemRow == col + row || itemCol - itemRow == col - row)){
+      board.push(col);
+      backTrack(result , n, board, row + 1);
+      board.pop();
+    }
+  }
+}
+
+let n = 3;
+let result = solveNQueens(n);
+console.log(result); 
