9bae360a6b3851a7f18ecddc96318f23e270bb0b
praga1125 <rpraga50@gmail.com>
2021-05-12 20:51:29 +0530

    restructing codebase

--- DSA/linked-list/mergeList.js
+++ DSA/linked-list/mergeList.js
@@ -1,51 +1,75 @@
- function Node(data, next) {
+class Node {
+  constructor(data){
     this.data = data;
-    this.next = next;
-  } 
-
-function merge(L1, L2) {
-    let L3 = new Node(null, null);
-    let prev = L3;
-    
-    while (L1 !== null && L2 !== null) {
-      if (L1.data <= L2.data) { 
-        prev.next = L1;
-        L1 = L1.next;
-      } else {
-        prev.next = L2;
-        L2 = L2.next;
-      }
-      prev = prev.next;
-    }
-    if (L1 === null){
-        prev.next = L2; 
-    }
-    if (L2 === null){ 
-        prev.next = L1; 
-    }
-    return L3;
-    
+    this.next = null;
   }
-function printList(result){ 
-    let curr = result.next; 
-    let str = ""; 
-    while (curr) { 
-        str += curr.data + " -> "; 
-        curr = curr.next; 
-    }
-    console.log(str); 
-    }
+}
 
-  let n3 = new Node(10, null);
-  let n2 = new Node(3, n3);
-  let n1 = new Node(1, n2);
-  let L1 = n1;
+class linkedList {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
   
-  let n6 = new Node(9, null);
-  let n5 = new Node(6, n6);
-  let n4 = new Node(5, n5);
-  let L2 = n4; 
-  
- let result = merge(L1, L2);
-console.log(`The merged two Linked List:`);
- printList(result);
+  insert(data) {
+    let node = new Node(data);
+    if(this.head == null){
+      this.head = node;
+    }
+    else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+
+  mergeList(list1, list2){
+    let result = new Node(null);
+    let current =  result;
+    while(list1 != null && list2 != null){
+      if(list1.data < list2.data){
+        current.next = list1;
+        list1 = list1.next;
+      }
+      else {
+        current.next = list2;
+        list2 = list2.next;
+      }
+      current = current.next;
+    }
+    if(list1 == null){
+      current = list2;
+    }
+    if(list2 == null){
+      current = list1;
+    }
+    return result.next;
+  }
+
+  printList(node){
+    let str = '';
+    while(node){
+      str += String(node.data) + " => ";
+      node = node.next;
+    }
+    console.log(str);
+  }
+
+}
+
+let obj1 = new linkedList();
+let obj2 = new linkedList();
+const list1 = [1, 2, 7];
+const list2 = [1, 4, 6, 8];
+for(let i = 0; i < list1.length; i++){
+  obj1.insert(list1[i]);
+}
+for(let j = 0; j < list2.length; j++){
+  obj2.insert(list2[j]);
+}
+
+let result = obj1.mergeList(obj1.head, obj2.head);
+console.log(`The given two linked list are :`);
+obj1.printList(obj1.head);
+obj1.printList(obj2.head);
+console.log(`The merged linked list is :`);
+obj1.printList(result);
