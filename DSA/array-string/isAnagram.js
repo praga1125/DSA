d608b3935eedd7f5046f7fa7a2d32f1af95c3e4d
praga1125 <rpraga50@gmail.com>
2021-05-12 22:12:18 +0530

    program of check the valid anagrams

---
+++ DSA/array-string/isAnagram.js
@@ -0,0 +1,29 @@
+function isAnagram(str1, str2) {
+  if(str1.length != str2.length){
+    return false;
+  }
+  let letters = new Array(28).fill(0), temp;
+  for(let i = 0; i < str1.length; i++){ 
+      temp = str1.charCodeAt(i) - 97;
+      letters[temp]++;
+  }
+  for(let i =0; i < str2.length; i++){
+    temp = str2.charCodeAt(i) - 97;
+    if(letters[temp] == 0){
+      return false;
+    }
+    letters[temp]--;
+  }
+  return true;
+}
+
+const str1 = "TON";
+const str2 = "NOT";
+console.log(`The given strings are : ${str1} and ${str2}`);
+const result = isAnagram(str1, str2);
+if(result){
+  console.log(`The Strings are valid anagrams`);
+}
+else {
+  console.log(`The Strings are not valid anagrams`);
+}
