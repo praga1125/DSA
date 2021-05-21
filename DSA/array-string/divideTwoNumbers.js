92cab9a586aa4c7317f13f33aeb216b57f41a595
praga1125 <rpraga50@gmail.com>
2021-05-21 21:00:04 +0530

    program of find the division of two numbers without using multple, division and mod operators

---
+++ DSA/array-string/divideTwoNumbers.js
@@ -0,0 +1,30 @@
+const divideTwoNumbers = (dividend, divisor) => {
+  if(dividend === -2147483648 && divisor === -1) 
+    return 2147483648;
+  let result = 0, sign = 1;
+  if(dividend < 0){
+    dividend = -dividend;
+    sign = -sign;
+  }
+  if(divisor < 0) {
+    divisor = -divisor;
+    sign = -sign;
+  }
+  if(dividend === divisor) {
+    return sign;
+  }
+  for(let i = 0, value = divisor; dividend >= divisor; i = 0, value = divisor) {
+    while( value > 0 && value <= dividend) {
+      value = divisor << ++i;
+      dividend -= divisor << i-1;
+      result += 1 << i-1;
+    } 
+  }
+  return sign < 0 ? -result : result;
+}
+
+const dividend = 7, divisor =  -3;
+console.log(`The dividend is : ${dividend}, divisor is : ${divisor}`);
+const result = divideTwoNumbers(dividend, divisor);
+console.log(`The division of the two numbers is : ${result}`);
+console.log('(without using multiplication, division, modulus operator)');
