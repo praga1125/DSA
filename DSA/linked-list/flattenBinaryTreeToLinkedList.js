4ec593c7277e1922f9e0832e3f1c03b884e7a87a
praga1125 <rpraga50@gmail.com>
2021-05-26 21:41:17 +0530

    program of convert a flatten binary tree to linkedList

--- DSA/linked-list/flattenBinaryTreeToLinkedList.js
+++ DSA/linked-list/flattenBinaryTreeToLinkedList.js
@@ -1,82 +1,27 @@
-class node{
-    constructor(data){
-        this.data = data;
-        this.left = null;
-        this.right = null;
+const copyRandomList = (head) => {
+    if (head == null) {
+        return null;
     }
-}
-class binaryTree{
-    constructor(){
-        this.root = null;
+    let temp = head;
+    while (temp !== null) {
+        let newNode = new Node(temp.val);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        temp = newNode.next;
     }
-
-    insert(data){
-        let newNode = new node(data);
-        if(this.root == null){
-            this.root = newNode;
-        } 
-        else{
-            this.insertNode(this.root, newNode);
-        }
+    temp = head;
+    while (temp !== null) {
+        temp.next.random = temp.random ? temp.random.next : null;
+        temp = temp.next.next;
     }
-    insertNode(node1, newNode){
-        if(newNode.data < node1.data){
-            if(node1.left == null){
-                node1.left = newNode;
-            }
-            else{
-                this.insertNode(node1.left, newNode);
-            }
-        }
-        else{
-            if(node1.right == null){
-                node1.right = newNode;
-            }
-            else{
-                this.insertNode(node1.right, newNode);
-            }
-        }
+    let oldList = head;
+    let newList = head.next;
+    let dummy = newList;
+    while (oldList !== null) {
+        oldList.next = oldList.next.next;
+        newList.next = newList.next ? newList.next.next : null;
+        oldList = oldList.next;
+        newList = newList.next;
     }
-
-    flatten(root) {
-        if (root === null){
-             return null;
-        }
-        let current = root;
-        while (current !== null){
-            if (current.left) {
-                var last = current.left;
-                while (last.right !== null){ 
-                    last = last.right;
-                }
-                var tmp = current.right;
-                current.right = current.left;
-                last.right = tmp;
-                current.left = null;
-            }
-            current = current.right;
-        }    
-        return root;
-    } 
-
-    printList(node){
-        let result = '';
-        while(node != null){
-        result += String(node.data) + ' => ';
-        node = node.right;
-        }
-        console.log(result);
-     }
-}
-
-let obj = new binaryTree();
-let arr = [1, 2, 5, 3, 4, 6];
-console.log(`The given binary tree values :`);
-console.log(arr);
-for(i of arr){
-    obj.insert(i);
-}
-console.log(`The flattened binary tree to linkedlist :`);
-let result = obj.flatten(obj.root);
-obj.printList(result);
-
+    return dummy;
+}   
