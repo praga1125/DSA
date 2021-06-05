52c6518a5cf8f145db4dd66056f9501872ceabc3
praga1125 <rpraga50@gmail.com>
2021-06-05 21:22:20 +0530

    refactor : updated codebase

--- DSA/linked-list/removeNthNode.js
+++ DSA/linked-list/removeNthNode.js
@@ -1,61 +1,61 @@
-class node{
-    constructor(data, next = null){
-        this.data = data;
-        this.next = next;
-    }
+class node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
 }
-class linkedList{
-    constructor(){
-        this.head = null;
-        this.size = 0;
-    }
-    addElements(elements){
-        let node1 = new node(elements);
-        if(this.head == null){
-            this.head = node1;
-        } else {
-       let current = this.head;
-       while(current.next){
-           current = current.next;
-       }    
-       current.next = node1;
-    }  
-    this.size++ ;
- }
+class linkedList {
+	constructor() {
+		this.head = null;
+		this.size = 0;
+	}
+	addElements(elements) {
+		let node1 = new node(elements);
+		if (this.head == null) {
+			this.head = node1;
+		} else {
+			let current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = node1;
+		}
+		this.size++;
+	}
 
- removeNthNode(head , target){
-    let left, before, right = head;
-  left = before = {next: head}; 
-  while (target--) 
-  right = right.next;
-  while (right) {
-    right = right.next;
-    left = left.next;
-  }
-  left.next = left.next.next;
-  return before.next;
-}
-printList(list){ 
-    let curr = list; 
-    let str = ""; 
-    while (curr) { 
-        str += curr.data + " -> "; 
-        curr = curr.next; 
-    }
-    console.log(str); 
-    } 
-
+	removeNthNode(head, target) {
+	let list = [];
+	let node = head;
+	while(node != null) {
+		list.push(node);
+		node = node.next;
+	}
+	let targetNode = list[list.length - target - 1];
+	if(target == list.length) {
+		head  = head.next;
+	}	else {
+		targetNode.next = targetNode.next == null ? null : targetNode.next.next;
+	}
+	return head;
+	}
+	printList(list) {
+		let curr = list;
+		let str = "";
+		while (curr) {
+			str += curr.data + " -> ";
+			curr = curr.next;
+		}
+		console.log(str);
+	}
 }
 
 const obj = new linkedList();
-obj.addElements(2);
-obj.addElements(1);
-obj.addElements(3);
-obj.addElements(6);
-obj.addElements(4);
+let arr = [2, 1, 3, 6, 4];
+for (let i of arr) {
+	obj.addElements(i);
+}
 console.log(`The given linked list :`);
-obj.printList(obj.head)
-let result = obj.removeNthNode(obj.head, 3);
+obj.printList(obj.head);
+let result = obj.removeNthNode(obj.head, 2);
 console.log(`The deleted linked list :`);
 obj.printList(result);
-
