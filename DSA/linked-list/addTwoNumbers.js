a6d33bd78a9485edb106a10e04a6fa20def8d26e
praga1125 <rpraga50@gmail.com>
2021-06-05 22:01:50 +0530

    refactor: updated code base

--- DSA/linked-list/addTwoNumbers.js
+++ DSA/linked-list/addTwoNumbers.js
@@ -1,72 +1,70 @@
 class Node {
-    constructor(value) {
-        this.data = value;
-        this.next = null;
-    }
+	constructor(value) {
+		this.data = value;
+		this.next = null;
+	}
 }
 class LinkedList {
-    constructor(head = null) {
-        this.head = head;
-    }
+	constructor(head = null) {
+		this.head = head;
+	}
 
-    addElements(elements){
-     let node1 = new Node(elements);
-      if(this.head == null){
-          this.head = node1;
-      } 
-			else {
-       let current = this.head;
-       while(current.next){
-         current = current.next;
-       }    
-       current.next = node1;
-    }
-    this.size++ ;
-  }
+	addElements(elements) {
+		let node1 = new Node(elements);
+		if (this.head == null) {
+			this.head = node1;
+		} else {
+			let current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = node1;
+		}
+		this.size++;
+	}
 	addTwoNumbers(l1, l2) {
-        let res = new Node(-1),
-        dummy = res,
-        sum = 0, carry = 0;
-    
-    while(l1 || l2 || sum > 0) {
-        if(l1) {
-            sum += l1.data;
-            l1 = l1.next;
-        }
-        
-        if(l2) {
-            sum += l2.data;
-            l2 = l2.next;
-        }
-        
-        if(sum >= 10) {
-            sum -= 10;
-            carry = 1;
-        }
-        
-        dummy.next = new Node(sum);
-        dummy = dummy.next;
-        sum = carry;
-        carry = 0;
-    }
-    return res.next;
-}
+		let temp = new Node();
+		let plusOne = 0;
+		let firstList = l1,
+			secondList = l2,
+			addedList = temp;
 
-  printList(node){
-  	let result = '';
-  	while(node != null){
-      result += String(node.data) + ' => ';
-      node = node.next;
-  	}
-  	console.log(result);
-   }
+		while (firstList || secondList) {
+			const sum =
+				((firstList && firstList.data) || 0) +
+				((secondList && secondList.data) || 0) +
+				plusOne;
+			if (sum >= 10) {
+				plusOne = 1;
+			} else {
+				plusOne = 0;
+			}
+			addedList.next = new Node(sum % 10);
+			addedList = addedList.next;
+			firstList = firstList && firstList.next;
+			secondList = secondList && secondList.next;
+		}
+		if (plusOne) {
+			addedList.next = new Node(1);
+		}
+		return temp.next;
+	}
+
+	printList(node) {
+		let result = "";
+		while (node != null) {
+			result += String(node.data) + " => ";
+			node = node.next;
+		}
+		console.log(result);
+	}
 }
 
 let arr1 = [5, 6, 7, 8, 9];
 let arr2 = [1, 2, 3, 4, 5];
 let obj1 = new LinkedList();
 let obj2 = new LinkedList();
-for(let i=0 ; i<arr1.length; i++){
+for (let i = 0; i < arr1.length; i++) {
 	obj1.addElements(arr1[i]);
 	obj2.addElements(arr2[i]);
 }
