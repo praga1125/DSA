34f257ee9e17f7a8e5613cde8263b27d0c91ef6a
praga1125 <rpraga50@gmail.com>
2021-06-07 22:15:31 +0530

    refactor : codebase

--- DSA/array-string/minWindow.js
+++ DSA/array-string/minWindow.js
@@ -1,32 +1,33 @@
-let minWindow = (arr, target) => {
-let array = new Array(128).fill(0);
-let result = [-Infinity, Infinity];
-let missing = target.length;
-for(let i=0;i<target.length;i++){
-    array[target.charCodeAt(i)]++;
-}
-let start = 0;
-for(let end=0;end<arr.length;end++){
-    if(array[arr.charCodeAt(end)] > 0){
-    missing--;
-    }
-    array[arr.charCodeAt(end)]--;
-    while(missing == 0){
-        if(result[1] - result[0] > end - start){
-            result[1] = end;
-            result[0] = start;
-        }
-        array[arr.charCodeAt(start)]++;
-        if(array[arr.charCodeAt(start)] > 0){
-            missing++;
-        }
-        start++;
-    }
-}
-return result[1] == Infinity ? "" : arr.slice(result[0], result[1]+1)
-}
+const minWindow = function (s, t) {
+	let arr = new Array(128).fill(0);
+	let result = [-Infinity, Infinity];
+	let missing = t.length;
+	for (let i = 0; i < t.length; i++) {
+		arr[t.charCodeAt(i)]++;
+	}
+	let start = 0;
+	for (let end = 0; end < s.length; end++) {
+		if (arr[s.charCodeAt(end)] > 0) {
+			missing--;
+		}
+		arr[s.charCodeAt(end)]--;
+		while (missing == 0) {
+			if (result[1] - result[0] > end - start) {
+				result[1] = end;
+				result[0] = start;
+			}
+			arr[s.charCodeAt(start)]++;
+			if (arr[s.charCodeAt(start)] > 0) {
+				missing++;
+			}
+			start++;
+		}
+	}
 
-const  arr = "helloWorld";
+	return result[1] == Infinity ? "" : s.slice(result[0], result[1] + 1);
+};
+
+const arr = "helloWorld";
 const target = "eoo";
 console.log(`The given String is : ${arr}`);
 console.log(`The target string : ${target}`);
