1552c5aace7233b07553c9636f24a82550e48aef
praga1125 <rpraga50@gmail.com>
2021-06-08 20:48:23 +0530

    refactor: updated codebase

--- DSA/array-string/nextGreaterElement.js
+++ DSA/array-string/nextGreaterElement.js
@@ -1,25 +1,21 @@
-function  nextGreaterElement(findNums, nums) {
-    return findNums.map(n => {
-        let found = nums.indexOf(n);
-        
-        if (found !== -1) {
-            while (nums[++found] < n);
-                if (found >= nums.length){
-                    found = -1;
-                }
-            else{
-                 found = nums[found];
-            }
-        }
-        
-        return found;
-    });
+function nextGreaterElement(nums1, nums2) {
+	let result = [];
+	nums1.forEach((num1) => {
+		let res = -1;
+		for (let i = nums2.indexOf(num1) + 1; i < nums2.length; i++) {
+			if (nums2[i] > num1) {
+				res = nums2[i];
+				break;
+			}
+		}
+		result.push(res);
+	});
+	return result;
 }
 
-let findNums = [1, 2];
+let findNums = [1, 6];
 console.log(`The find numbers are : ${findNums}`);
 let nums = [5, 1, 6, 2, 0];
 console.log(`The given numbers are : ${nums}`);
 let result = nextGreaterElement(findNums, nums);
 console.log(`The next greater elements are : ${result}`);
-
