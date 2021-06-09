a84d215e07142e86a8637cd71657b78afabf1908
praga1125 <rpraga50@gmail.com>
2021-06-09 22:12:36 +0530

    refactor : updated codebase

--- DSA/array-string/sort012.js
+++ DSA/array-string/sort012.js
@@ -1,27 +1,22 @@
-function sorted(arr){
-    let low = 0, high = arr.length-1, mid = 0;
-    let temp = 0;
-    while(mid <= high){
-        if(arr[mid] == 0){
-            temp = arr[low];
-            arr[low] = arr[mid];
-            arr[mid] = temp;
-            low++;
-            mid++;
-        }
-        else if(arr[mid] == 1){
-            mid++;
-        }
-        else{
-            temp = arr[mid];
-            arr[mid] = arr[high];
-            arr[high] = temp;
-            high--;
-        }
-    }
+function sorted(arr) {
+	let low = 0,
+		  high = arr.length - 1,
+		  mid = 0;
+	while (mid <= high) {
+		if (arr[mid] == 0) {
+			[arr[low], arr[mid]] = [arr[mid], arr[low]];
+			low++;
+			mid++;
+		} else if (arr[mid] == 1) {
+			mid++;
+		} else {
+			[arr[mid], arr[high]] = [arr[high], arr[mid]];
+			high--;
+		}
+	}
 }
 
-let arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 ];
+let arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1];
 console.log(`The given array is : ${arr}`);
 sorted(arr);
 console.log(`The sorted array is : ${arr}`);
