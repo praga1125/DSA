b75e85b5a34417a14c5342057820bdbabf673f0c
praga1125 <rpraga50@gmail.com>
2021-06-16 22:12:20 +0530

    refactor : updated codebase

--- DSA/tree/inorderTraversal.js
+++ DSA/tree/inorderTraversal.js
@@ -1,64 +1,43 @@
-class node{
-    constructor(data){
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+class Node {
+	constructor(data) {
+		this.value = data;
+		this.left = null;
+		this.right = null;
+	}
 }
-class binarySearchTree{
-    constructor(){
-        this.root = null;
-    }
-  insert(data){
-      let newNode = new node(data);
-      if(this.root == null){
-          this.root = newNode;
-      } 
-      else{
-          this.insertNode(this.root, newNode);
-      }
-    }
-  insertNode(node1, newNode){
-      if(newNode.data < node1.data){
-          if(node1.left == null){
-              node1.left = newNode;
-          }
-          else{
-              this.insertNode(node1.left, newNode);
-          }
-      }
-      else{
-          if(node1.right == null){
-              node1.right = newNode;
-          }
-          else{
-              this.insertNode(node1.right, newNode);
-          }
-       }
+class binarySearchTree {
+  constructor () {
+    this.ans = [];
   }
-  inorder(root){
-		let stack = [];
-		let result = [];
-		while(root || stack.length){
-			while(root){
-				stack.push(root);
-				root = root.left;
-				}
-				root = stack.pop();
-				result.push(root.data);
-				root = root.right;
-				}
-			return result;
-    }
+	insert(root, data) {
+		if (root == null) {
+			root = new Node(data, null, null);
+		} else if (data < root.value) {
+			root.left = this.insert(root.left, data);
+		} else {
+			root.right = this.insert(root.right, data);
+		}
+		return root;
+	}
+
+	inOrder(node) {
+		if (node !== null) {
+			this.inOrder(node.left);
+      this.ans.push(node.value);
+			this.inOrder(node.right);
+		}
+    return this.ans;
+	}
 }
 
 let obj = new binarySearchTree();
 let arr = [1, 3, 2, 4, 0];
+let root;
 console.log(`The given binary tree is :`);
 console.log(arr);
-for(i of arr){
-	obj.insert(i);
+for (i of arr) {
+	root = obj.insert(root, i);
 }
-let result = obj.inorder(obj.root);
-console.log(`The inorder traversal of BST is :`);
+let result = obj.inOrder(root);
+console.log(`The inOrder traversal of BST is :`);
 console.log(result);
