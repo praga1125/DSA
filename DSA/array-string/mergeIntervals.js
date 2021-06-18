34305099de79f8034b4aa0dcba121874bf7236b6
praga1125 <rpraga50@gmail.com>
2021-06-18 20:37:48 +0530

    refactor : updated codebase

--- DSA/array-string/mergeIntervals.js
+++ DSA/array-string/mergeIntervals.js
@@ -3,27 +3,34 @@ INPUT: [[1,3], [2, 4], [2, 6], [8, 9], [8, 10], [9, 11], [15, 18], [16, 17]]
 OUTPUT: [[1,6], [8,11], [15,18]]
 */
 const mergeIntervals = (intervals) => {
-    let result = [];
-    let first = null, last =null;
-    for(let i = 0; i < intervals.length; i++){
-    if(!first){
-        first = intervals[i][0];
-        last = intervals[i][1];
-    }
-    if(intervals[i][0] <= last){
-        if(intervals[i][1] > last){
-            last = intervals[i][1];
-        }
-    } else {
-        result.push([first, last]);
-        first = intervals[i][0];
-        last = intervals[i][1];
-    }
-  }
-  result.push([first, last]);
-  return result;
-}
+	if (!intervals.length) return intervals;
+	intervals.sort((a, b) => (a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]));
+	let prev = intervals[0];
+  // console.log(prev);
+	const result = [prev];
+	for (const curr of intervals) {
+		if (curr[0] <= prev[1]) {
+			prev[1] = Math.max(curr[1], prev[1]);
+		} else {
+			result.push(curr);
+			prev = curr;
+		}
+	}
+	return result;
+};
 
-const intervals = [[1,3], [2, 4], [2, 6], [8, 9], [8, 10], [9, 11], [15, 18], [16, 17]];
+const intervals = [
+	[1, 3],
+	[2, 4],
+	[2, 6],
+	[8, 9],
+	[8, 10],
+	[9, 11],
+	[15, 18],
+	[16, 17],
+];
+console.log(`The given intervals are :`);
+console.log(intervals);
 const mergedIntervals = mergeIntervals(intervals);
+console.log(`The merged intervals are :`);
 console.log(mergedIntervals);
