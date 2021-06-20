a716f84743b49ec94fc824225492f84feda921fb
praga1125 <rpraga50@gmail.com>
2021-06-21 05:24:10 +0530

    refactor : updated : codebase

--- DSA/array-string/myPow.js
+++ DSA/array-string/myPow.js
@@ -1,22 +1,13 @@
-function myPow(x, y) {
-	if (y == 0) {
-		return 1;
-	} else if (y == 1) {
-		return x;
-	} else if (y == -1) {
-		return 1 / x;
-	} else if (y % 2 == 0) {
-		let result = myPow(x, y / 2);
-		return result * result;
-	} else if (y % 2 != 0) {
-		return x * myPow(x, y - 1);
-	} else {
-		console.log(`incorrect input..`);
-	}
+function myPow(x, n) {
+	if (n === 0) return 1;
+	if (n === 1) return x;
+	if (n < 0) return 1 / myPow(x, -n);
+	if (n % 2 === 0) return myPow(x * x, n / 2);
+	return x * myPow(x * x, (n - 1) / 2);
 }
 
-let num1 = 2.1;
-let num2 = 3;
+let num1 = 2.0;
+let num2 = 10;
 console.log(`The given values are :`);
 console.log(`num1 : ${num1} , num2 : ${num2}`);
 let result = myPow(num1, num2);
