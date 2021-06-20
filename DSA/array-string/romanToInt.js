5140ce964467980e73616ee20e052c4695c3a163
praga1125 <rpraga50@gmail.com>
2021-06-20 12:17:28 +0530

    refactor : updated codebase

--- DSA/array-string/romanToInt.js
+++ DSA/array-string/romanToInt.js
@@ -1,21 +1,18 @@
-function romanToInt(str){
-    if(str.length == 0){
-        return 0;
-    }
-    let obj = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
-    let result = 0;
-    for(let i=0; i<str.length; i++){
-        if(obj[str[i]] < obj[str[i+1]]){
-            result = result + (obj[str[i+1]] - obj[str[i]]);
-        }
-        else {
-            result += obj[str[i]];
-        }
-    }
-    return result;
+function romanToInt(str) {
+	let result = 0;
+	const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+	str = str.split("");
+	for (let i = 0; i < str.length; i++) {
+		if (str[i + 1] && map[str[i]] < map[str[i + 1]]) {
+			result -= map[str[i]];
+		} else {
+			result += map[str[i]];
+		}
+	}
+	return result;
 }
 
-let str = 'XII';
+let str = "IV";
 console.log(`The given roman letter is : ${str}`);
 let result = romanToInt(str);
 console.log(`The roman letter to the Integer is : ${result}`);
