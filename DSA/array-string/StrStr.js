05696756fb939ebf05d90201994e2ac7dc362191
praga1125 <rpraga50@gmail.com>
2021-06-21 19:38:42 +0530

    refactor : updated codebase

--- DSA/array-string/StrStr.js
+++ DSA/array-string/StrStr.js
@@ -1,23 +1,18 @@
-function StrStr(str, pattern){
-  if(pattern == '' || str == pattern){
-    return 0;
-  }
-  if(str.length < pattern.length){
-    return -1;
-  }
-  for(let i=0; i<str.length-pattern.length+1; i++){
-    if(str[i] == pattern[0]){
-      for(let j=0; j<pattern.length; j++){
-        if(pattern[j] != str[i+j]){
-          break;
-        }
-        else if(j == pattern.length-1){
-          return i;
-        }
-      }
-    }
-  }
-    return -1;
+function StrStr(str, pattern) {
+	let ptr = 0;
+	let result = -1;
+	if (str.length == "" && pattern.length == "") {
+		return 0;
+	} else {
+		while (ptr < str.length) {
+			if (str.slice(ptr, ptr + pattern.length) == pattern) {
+				result = ptr;
+				break;
+			}
+			ptr++;
+		}
+		return result;
+	}
 }
 
 let str = "helloPraga";
@@ -25,9 +20,8 @@ console.log(`The given string is : ${str}`);
 let pattern = "aga";
 console.log(`The given pattern string is : ${pattern}`);
 let result = StrStr(str, pattern);
-if(result != -1){
-  console.log(`The string and pattern is matched in index of : ${result}`);
-}
-else{
-  console.log(`The string and pattern are not matched`);
+if (result != -1) {
+	console.log(`The string and pattern is matched in index of : ${result}`);
+} else {
+	console.log(`The string and pattern are not matched`);
 }
