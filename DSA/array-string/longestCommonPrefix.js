f559cefa9721ab162e0dfaecdbacab986fc14944
praga1125 <rpraga50@gmail.com>
2021-06-27 21:17:19 +0530

    refactor : updated codebase

--- DSA/array-string/longestCommonPrefix.js
+++ DSA/array-string/longestCommonPrefix.js
@@ -1,13 +1,13 @@
 function longestCommonPrefix(str) {
-	if (str.length == 0 || str.length < 0 || str.length > 200) return "";
-	let prefix = str[0];
-	for (let i = 0; i < str.length; i++) {
-		while (str[i].indexOf(prefix) !== 0) {
-			prefix = prefix.substring(0, prefix.length - 1);
-			if (prefix == "") return "";
-		}
-	}
-	return prefix;
+	let len = str.length;
+	if (len == 0) return "";
+	if (len == 1) return str[0];
+	str.sort();
+	let min = Math.min(str[0].length, str[len - 1].length);
+	let i = 0;
+	while (i < min && str[0][i] == str[len - 1][i]) i++;
+	let pre = str[0].substring(0, i);
+	return pre;
 }
 
 let str = ["flip", "flop", "flower", "flow"];
