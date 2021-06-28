b443414ed9f0bd091cb725271a83e74f665b566f
praga1125 <rpraga50@gmail.com>
2021-06-28 09:14:54 +0530

    refactor : updated codebase

--- DSA/array-string/removeDuplicates.js
+++ DSA/array-string/removeDuplicates.js
@@ -1,19 +1,17 @@
-function removeDuplicates(arr){
-  let obj = {};
-  let result = [];
-  for(let i=0; i<arr.length;i++){
-    if(obj[arr[i]] == 0){
-        obj[arr[i]] = 1;
-    }
-    else {
-      obj[arr[i]] = 0;
-      result.push(arr[i]);
-    }
+function removeDuplicates(nums) {
+	let set = new Set(nums);
+  let arr = Array.from(set);
+  let temp = nums.length - arr.length;
+  for(let i = 0; i < arr.length; i++) {
+    nums[i] = arr[i];
   }
-  return result;
-}
+  for(let i = 0; i < temp; i++) {
+    nums.pop();
+  }
+  return nums;
+} 
 
-let arr = [1, 3, 4, 5, 1, 2, 3];
+let arr = [1, 1, 2, 2, 4, 4, 7, 7];
 console.log(`The given array is : ${arr}`);
 let result = removeDuplicates(arr);
 console.log(`The removed duplicates array is : ${result}`);
