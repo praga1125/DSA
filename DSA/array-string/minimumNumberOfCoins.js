d399c27eb45a081d3c9d0cdf3e285ffccb702bf6
praga1125 <rpraga50@gmail.com>
2021-06-30 21:04:35 +0530

    refactor : codebase

--- DSA/array-string/minimumNumberOfCoins.js
+++ DSA/array-string/minimumNumberOfCoins.js
@@ -1,18 +1,17 @@
 function minimumNumberOfCoins(coins, amount) {
-  if(!amount) return 0;
-  let dp = new Array(amount + 1).fill(Number.MAX_VALUE);
-  dp[0] = 0; 
-  for(let i = 0; i < coins.length; i++) {
-    let coin = coins[i];
-    for(let j = 1; j <= amount; j++) {
-      if(coin <= j)
-        let diff = dp[j - coin];
-      if(diff != -1 && diff + 1 < dp[j]) {
-        dp[j] = diff + 1;
-      }
-    }
-  }
-  return dp[amount] === Number.MAX_VALUE ? -1 : dp[amount];
+	if (!amount) return 0;
+	let dp = new Array(amount + 1).fill(Number.MAX_VALUE);
+	dp[0] = 0;
+	for (let i = 0; i < coins.length; i++) {
+		let coin = coins[i];
+		for (let j = 1; j <= amount; j++) {
+			if (coin <= j) {
+				let diff = dp[j - coin];
+				if (diff !== -1 && diff + 1 < dp[j]) dp[j] = 1 + diff;
+			}
+		}
+	}
+	return dp[amount] === Number.MAX_VALUE ? -1 : dp[amount];
 }
 
 const coins = [1, 2, 5];
