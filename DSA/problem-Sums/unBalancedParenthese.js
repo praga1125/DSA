f24ba3dfc000f50f435c47cd97c2c14f551d9b09
praga1125 <rpraga50@gmail.com>
2021-07-20 21:09:28 +0530

    set 2 : remove the unbalanced parentheses in a expression

---
+++ DSA/problem-Sums/unBalancedParenthese.js
@@ -0,0 +1,32 @@
+let str = "((abc)((de))";
+let temp = [], count = 0;
+let n = str.length;
+let result = '';
+for(let i = 1; i < n-1; i++) {
+	if(str[i] == '(') {
+		temp[count] = i;
+		count++;
+	}
+	if(str[i] == ')') {
+		if(count != 0) {
+			count--;
+		} else {
+			temp[count] = i;
+			count++;
+		}
+	}
+}
+
+for(let i = 0; i < n; i++) {
+	let flag = 0;
+	for(let j = 0; j < count; j++) {
+		if(i == temp[j]) 
+			flag = 1;
+	}
+		if(flag == 0) {
+			result += str[i];
+			}
+}
+
+console.log(`The given string is : ${str}`);
+console.log(`The balanced parentheses of string : ${result}`);
