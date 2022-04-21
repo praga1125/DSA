e2b0cfb6daa5e8092a4b800eca147fa55166430c
praga1125 <rpraga50@gmail.com>
2022-04-21 21:09:29 +0530

    find the alternative sorting

--- DSA/problem-Sums/alternativeSorting.js
+++ DSA/problem-Sums/alternativeSorting.js
@@ -1,17 +1,17 @@
-let arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(`The given array is :`);
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(`The given input is :`);
 console.log(arr);
 arr.sort((a, b) => a - b);
 let result = [];
 let a = 0,
-	b = arr.length - 1;
+  b = arr.length - 1;
 while (result.length < arr.length) {
-	result.push(arr[b]);
-	if (a != b) {
-		result.push(arr[a]);
-	}
-	a++;
-	b--;
+  result.push(arr[b]);
+  if (a != b) {
+    result.push(arr[a]);
+  }
+  a++;
+  b--;
 }
 console.log(`The alternative sorting array is : `);
 console.log(result);
