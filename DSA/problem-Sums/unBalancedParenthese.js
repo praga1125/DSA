48cb23e3e4152bb40ed08947e05e41cc26077e45
praga1125 <rpraga50@gmail.com>
2022-04-23 23:31:13 +0530

    find the unbalanced paranthese

--- DSA/problem-Sums/unBalancedParenthese.js
+++ DSA/problem-Sums/unBalancedParenthese.js
@@ -1,8 +1,8 @@
 let str = "((abc)((de))";
 let temp = [], count = 0;
-let n = str.length;
+let len = str.length;
 let result = '';
-for(let i = 1; i < n-1; i++) {
+for(let i = 1; i < len-1; i++) {
 	if(str[i] == '(') {
 		temp[count] = i;
 		count++;
@@ -17,7 +17,7 @@ for(let i = 1; i < n-1; i++) {
 	}
 }
 
-for(let i = 0; i < n; i++) {
+for(let i = 0; i < len; i++) {
 	let flag = 0;
 	for(let j = 0; j < count; j++) {
 		if(i == temp[j]) 
